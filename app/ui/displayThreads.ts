3187b5fc82c285f07c8b6ff3edf85685960be81e
ltiret <liam.tiret@gmail.com>
2025-01-31 10:23:49 +0100

    bugs
    bugs

--- app/ui/displayThreads.ts
+++ app/ui/displayThreads.ts
@@ -28,48 +28,63 @@ export function displayThreads(threads: any[]): void {
       snippetCell.classList.add('UNREAD');
       const bluedot = document.createElement('span');
       snippetCell.appendChild(bluedot);
+      bluedot.classList.add('UNREAD');
+      bluedot.classList.add('dot');
+
       const UNREADCell = document.createElement('td');
+      threadIdCell.classList.add('UNREAD');
+      nbMessages.classList.add('UNREAD');
+      UNREADCell.classList.add('UNREAD');
+      row.appendChild(UNREADCell);
 
       const markasread = document.createElement('button');
       markasread.classList.add('button');
       markasread.textContent = 'Mark as read';
       UNREADCell.appendChild(markasread);
+
       markasread.addEventListener('click', function () {
-        snippetCell.removeChild(bluedot);
+        bluedot.classList.remove('UNREAD');
+        bluedot.classList.remove('dot');
+        document.createElement('button');
+        markasread.classList.add('button');
+        markasread.textContent = 'Mark as unread';
         UNREADCell.classList.add('READ');
         snippetCell.classList.add('READ');
         threadIdCell.classList.add('READ');
         nbMessages.classList.add('READ');
-        document.createElement('button');
-        markasread.classList.add('button');
-        markasread.textContent = 'Mark as unread';
-
         markAsRead(thread.id);
       });
-      threadIdCell.classList.add('UNREAD');
-      nbMessages.classList.add('UNREAD');
-      UNREADCell.classList.add('UNREAD');
-      bluedot.classList.add('dot');
-      row.appendChild(UNREADCell);
     } else {
-      const readCell = document.createElement('td');
-      row.appendChild(readCell);
       const bluedot = document.createElement('span');
-
-      row.classList.add('READ');
       const markasunread = document.createElement('button');
       markasunread.classList.add('button');
       markasunread.textContent = 'Mark as unread';
+      bluedot.classList.add('UNREAD');
+      bluedot.classList.add('dot');
+      const readCell = document.createElement('td');
+      row.appendChild(readCell);
+      row.classList.add('READ');
       readCell.appendChild(markasunread);
+
       markasunread.addEventListener('click', function () {
+        const bluedot = document.createElement('span');
+        bluedot.classList.add('UNREAD');
+        bluedot.classList.add('dot');
+        snippetCell.classList.add('UNREAD');
+        const markasread = document.createElement('button');
+        const UNREADCell = document.createElement('td');
+        UNREADCell.classList.remove('READ');
         readCell.classList.add('UNREAD');
+        markasread.classList.add('button');
+        markasread.textContent = 'Mark as read';
+
+        UNREADCell.appendChild(markasread);
+
         snippetCell.classList.add('UNREAD');
         threadIdCell.classList.add('UNREAD');
         nbMessages.classList.add('UNREAD');
-        document.createElement('UNbutton');
-        markasunread.classList.add('UNbutton');
-        markasunread.textContent = 'Mark as read';
-        snippetCell.appendChild(bluedot);
+        markasunread.classList.add('button');
+        snippetCell.classList.add('UNREAD');
 
         markAsUnread(thread.id);
       });
